f0a950130110698731c61e549f7acb8f9d118340
nachomonllor <nachomonllorc@gmail.com>
2024-04-09 20:07:56 -0300

    botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeraApp';
+  title = 'Mi nuevo titulo';
+
+  suma: number = 0;
+
+  nombre: string = "Augusto";
+
+  sumar() {
+    this.suma ++;
+  }
+  restar() {
+    if(this.suma > 0) {
+      this.suma--;
+    }
+  }
 }
